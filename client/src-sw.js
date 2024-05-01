6ec167ca0102064743e7e2c9fdbdbbc2dc9caaca
mayachakra <mayajchakravarty@gmail.com>
2024-05-01 12:12:05 -0400

    added changes

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,1 +1,21 @@
 // TODO: Create a service worker that caches static assets:
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
+import { precacheAndRoute } from 'workbox-precaching';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+// Register route for caching static assets
+registerRoute(
+    /\.(?:js|css|html|png|jpg|jpeg|svg|gif)$/,
+    new CacheFirst({
+    cacheName: 'static-assests-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// TODO: Require the InjectManifest class of the WorkBoxPlugin 
+const {InjectManifest} = require('workbox-webpack-plugin');
+
 const path = require('path');
 
 module.exports = () => {
@@ -11,17 +16,46 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+     // TODO: Add InjectManifest Plugin
+     new InjectManifest({
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
+    }), 
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
     }
   };
 };
